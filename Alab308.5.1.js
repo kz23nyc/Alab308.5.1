ec035a495d62bef42257ec07debad21c84a392c0
kzy1112 <152929717+kz23nyc@users.noreply.github.com>
2024-08-13 23:04:03 -0400

    Update Alab308.5.1.js

--- Alab308.5.1.js
+++ Alab308.5.1.js
@@ -1,13 +1,33 @@
+//Part 1: Thinking Functionally
+//Take an array of numbers and return the sum.
 
-function calculateSum(numbers) {
-    // Using the reduce method for a concise solution
-    return numbers.reduce((sum, number) => sum + number, 0);
+  function sumArray(numbers) {
+    return numbers.reduce((sum, num) => sum + num, 0);
   }
   
-  console.log(calculateSum([1,2,3,4,5]));
+   console.log(sumArray([12,13,14,15,16])); //71
 
-  function calculateAverage(numbers) {
-    const sum = calculateSum(numbers); // Reuse the sum function
+// Take an array of numbers and return the average.
+
+function averageArray(numbers) {
+    const sum = sumArray(numbers); 
+    // Reuse the sumArray function
     return sum / numbers.length;
+}
+  console.log(averageArray([12,13,14,15,16])); //14
+
+// Take an array of strings and return the longest string.
+
+function findLongestString(strings) {
+    return strings.reduce((longest, current) => current.length > longest.length ? current : longest, "");
   }
-  console.log(calculateAverage([1,2,3,4,5]));
+  console.log(findLongestString(['Cherry','Watermelon','Apricot','Raspberry','Avocado'])); //Watermelon
+  
+
+// Take an array of strings, and a number and return an array of the strings that are longer than the given number. 
+
+function stringsLongerThan(strings) {
+    return strings}
+
+console.log(findLongestString(['Say','Hello beautiful','in','the','Good morning'])); //Hello beautiful
+  
